c7d02058ce18adc18c51a054be9511cc774b45de
Tobias Kunz <Tobias.Kunz@tngtech.com>
2020-07-20 22:30:21 +0200

    colors

--- src/components/DirectBuyComponent.tsx
+++ src/components/DirectBuyComponent.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+import './Polaroid.css'
+import {Typography} from "@material-ui/core";
 import {removeHashtag} from "../utils/Hashtag";
-import {checkIfPriceExists} from "../utils/Price";
 import {checkIfSizeExists} from "../utils/Size";
-
+import {checkIfPriceExists} from "../utils/Price";
+// import Typography from "@material-ui/core/Typography";
+// import Card from "@material-ui/core/Card";
+// import CardContent from "@material-ui/core/CardContent";
+// import {removeHashtag} from "../utils/Hashtag";
+// import {checkIfPriceExists} from "../utils/Price";
+// import {checkIfSizeExists} from "../utils/Size";
+// <div>
+//     <Card>
+//         <CardContent>
+//             <img src={"content/PolaroidFrame.png"} alt={"Polaroid"} className={'polaroid'}/>
+//             <img src={props.img} alt={"Product"} className={'polaroidimage'}/>
+//             <Typography>
+//                 {removeHashtag(props.description)}
+//             </Typography>
+//             <Typography>
+//                 {checkIfSizeExists(props.size)}
+//             </Typography>
+//             <Typography>
+//                 {checkIfPriceExists(props.price)}
+//             </Typography>
+//             <Typography>
+//                 {props.description}
+//             </Typography>
+//         </CardContent>
+//     </Card>
+// </div>
 
 interface DirectBuyComponentProps {
     img: string;
@@ -18,24 +42,27 @@ export default function DirectBuyComponent(props: DirectBuyComponentProps) {
 
     return (
         <div>
-            <Card>
-                <CardContent>
-                    {/*<img src={"content/PolaroidFrame.png"} alt={"Frame"}/>*/}
-                    {/*<img src={props.img} alt={"Product"}/>*/}
-                    <Typography>
-                        {removeHashtag(props.description)}
-                    </Typography>
-                    <Typography>
-                        {checkIfSizeExists(props.size)}
-                    </Typography>
-                    <Typography>
-                        {checkIfPriceExists(props.price)}
-                    </Typography>
-                    <Typography>
-                        {props.description}
-                    </Typography>
-                </CardContent>
-            </Card>
+            <span className="wrapper">
+                <span className="item">
+                    <span className="polaroid">
+                       <img src={props.img} alt={"Product"} />
+                            <span className="caption">
+                                 <Typography>
+                                    {checkIfSizeExists(props.size)}
+                                 </Typography>
+                                <Typography>
+                                     {checkIfPriceExists(props.price)}
+                                </Typography>
+                                <Typography>
+                                    {props.description}
+                                </Typography>
+                            </span>
+                    </span>
+                </span>
+            </span>
+
         </div>
     );
 }
+
+
